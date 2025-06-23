2fb2828729b7437321a89bf9abc4c439d91a501d
matheusgmdr7 <matheusgmdr7@gmail.com>
2025-06-23 11:53:38 -0300

    Revert "main"
    This reverts commit 4d79c3bda456f884a5056d65692b4f2befbe2da8.

--- contratandoplanosmain21 (3)/app/admin/ferramentas/analisar-fluxo-propostas/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Spinner } from "@/components/ui/spinner"
-
-export default function Loading() {
-  return (
-    <div className="flex justify-center items-center min-h-screen">
-      <Spinner />
-    </div>
-  )
-}
--- contratandoplanosmain21 (3)/app/admin/ferramentas/investigar-documentos/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Spinner } from "@/components/ui/spinner"
-
-export default function Loading() {
-  return (
-    <div className="flex justify-center items-center min-h-[400px]">
-      <Spinner />
-    </div>
-  )
-}
